97e843f9cdd86b346253c53daecfde1fae4bb5ee
bitrix <bitrix@www.shtrih-m.ru>
2020-02-03 08:26:02 +0300

    изменение страницы контактов

--- about/index.php
+++ about/index.php
@@ -3,9 +3,37 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("О компании");
 ?>
 <p align="justify" class="last"><img src="/upload/medialibrary/614/map_final_2012_shtrih_02_80.jpg" title="map_final_2012_shtrih_02_80.jpg" border="0" alt="map_final_2012_shtrih_02_80.jpg" width="640" height="480"  /></p>
- Время работы центрального офиса: с 10.00 до 19.00 без обеда, кроме субботы и воскресенья.. 
-<p></p>
- 
+    <p align="justify" class="last"><b>Офисы компании &quot;ШТРИХ-М&quot;</b></p>
+
+    <p align="justify" class="last"> <b>Центральный офис
+            <br />
+        </b>115280, Москва, улица Ленинская Слобода, дом 19, стр. 4. Метро &quot;Автозаводская&quot;.
+        <br />
+
+        <br />
+        <b>Телефоны:   </b>
+        <br />
+    </p>
+
+    <p align="justify" class="last">Отдел продаж: (495) 787-6090
+        <br />
+        Факс: (495) 787-6099 
+        <br />
+    </p>
+
+    <p align="justify" class="last"><b>Электронная почта: </b></p>
+
+    <p align="justify" class="last">Общий адрес: info@shtrih-m.ru
+        <br />
+        Web-мастер: site@shtrih-m.ru
+        <br />
+        Отдел продаж: sales@shtrih-m.ru
+        <br />
+        Техническая поддержка: support@shtrih-m.ru
+        <br />
+    </p>
+
+    <p align="justify">Время работы центрального офиса: с 10.00 до 19.00 без обеда, кроме субботы и воскресенья. </p>
 <p align="justify"><b>ГРУППА КОМПАНИЙ &laquo;ШТРИХ-М&raquo;</b></p>
  
 <p align="justify"><b>НАША МИССИЯ</b></p>
@@ -63,34 +91,5 @@ $APPLICATION->SetTitle("О компании");
 <p align="justify"> 
   <br />
  </p>
- 
-<p align="justify" class="last"><b>Офисы компании &quot;ШТРИХ-М&quot;</b></p>
- 
-<p align="justify" class="last"> <b>Центральный офис 
-    <br />
-   </b>115280, Москва, улица Ленинская Слобода, дом 19, стр. 4. Метро &quot;Автозаводская&quot;. 
-  <br />
- 
-  <br />
- <b>Телефоны:   </b> 
-  <br />
- </p>
- 
-<p align="justify" class="last">Отдел продаж: (495) 787-6090; 
-  <br />
- Факс: (495) 787-6099;  
-  <br />
- </p>
- 
-<p align="justify" class="last"><b>Электронная почта: </b></p>
- 
-<p align="justify" class="last">Общий адрес: info@shtrih-m.ru 
-  <br />
- Web-мастер: site@shtrih-m.ru 
-  <br />
- Отдел продаж: sales@shtrih-m.ru 
-  <br />
- Техническая поддержка: support@shtrih-m.ru 
-  <br />
- </p>
+
  <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
