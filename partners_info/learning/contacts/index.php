7c6d6a4adf429b4030255d8f9d39a262c9d93dc4
bitrix <bitrix@www.shtrih-m.ru>
2020-02-03 08:50:47 +0300

    add information on page contact learning

--- partners_info/learning/contacts/index.php
+++ partners_info/learning/contacts/index.php
@@ -1,57 +1,70 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты учебного центра");?>
-<?$APPLICATION->IncludeComponent("areal:filter_learning_contacts", "");?>
-<?$APPLICATION->IncludeComponent("bitrix:news.list", "learning_contacts", array(
-	"IBLOCK_TYPE" => "learning",
-	"IBLOCK_ID" => "29",
-	"NEWS_COUNT" => "20",
-	"SORT_BY1" => "SORT",
-	"SORT_ORDER1" => "ASC",
-	"SORT_BY2" => "",
-	"SORT_ORDER2" => "ASC",
-	"FILTER_NAME" => "arrFilter",
-	"FIELD_CODE" => array(
-		0 => "",
-		1 => "",
-	),
-	"PROPERTY_CODE" => array(
-		0 => "HEAD",
-		1 => "ADDRESS",
-		2 => "PHONE",
-		3 => "EMAIL",
-		4 => "CITY",
-		5 => "SCHEME",
-		6 => "",
-	),
-	"CHECK_DATES" => "Y",
-	"DETAIL_URL" => "",
-	"AJAX_MODE" => "N",
-	"AJAX_OPTION_JUMP" => "N",
-	"AJAX_OPTION_STYLE" => "Y",
-	"AJAX_OPTION_HISTORY" => "N",
-	"CACHE_TYPE" => "A",
-	"CACHE_TIME" => "36000000",
-	"CACHE_FILTER" => "N",
-	"CACHE_GROUPS" => "Y",
-	"PREVIEW_TRUNCATE_LEN" => "",
-	"ACTIVE_DATE_FORMAT" => "d.m.Y",
-	"SET_TITLE" => "N",
-	"SET_STATUS_404" => "N",
-	"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-	"ADD_SECTIONS_CHAIN" => "N",
-	"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-	"PARENT_SECTION" => "",
-	"PARENT_SECTION_CODE" => "",
-	"DISPLAY_TOP_PAGER" => "N",
-	"DISPLAY_BOTTOM_PAGER" => "Y",
-	"PAGER_TITLE" => "Новости",
-	"PAGER_SHOW_ALWAYS" => "N",
-	"PAGER_TEMPLATE" => "",
-	"PAGER_DESC_NUMBERING" => "N",
-	"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-	"PAGER_SHOW_ALL" => "Y",
-	"AJAX_OPTION_ADDITIONAL" => ""
-	),
-	false
-);?>
+	<!--</?$APPLICATION->IncludeComponent("areal:filter_learning_contacts", "");?>
+    </?$APPLICATION->IncludeComponent("bitrix:news.list", "learning_contacts", array(
+        "IBLOCK_TYPE" => "learning",
+        "IBLOCK_ID" => "29",
+        "NEWS_COUNT" => "20",
+        "SORT_BY1" => "SORT",
+        "SORT_ORDER1" => "ASC",
+        "SORT_BY2" => "",
+        "SORT_ORDER2" => "ASC",
+        "FILTER_NAME" => "arrFilter",
+        "FIELD_CODE" => array(
+            0 => "",
+            1 => "",
+        ),
+        "PROPERTY_CODE" => array(
+            0 => "HEAD",
+            1 => "ADDRESS",
+            2 => "PHONE",
+            3 => "EMAIL",
+            4 => "CITY",
+            5 => "SCHEME",
+            6 => "",
+        ),
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "AJAX_OPTION_HISTORY" => "N",
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "36000000",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "Y",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "SET_TITLE" => "N",
+        "SET_STATUS_404" => "N",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+        "ADD_SECTIONS_CHAIN" => "N",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "PARENT_SECTION" => "",
+        "PARENT_SECTION_CODE" => "",
+        "DISPLAY_TOP_PAGER" => "N",
+        "DISPLAY_BOTTOM_PAGER" => "Y",
+        "PAGER_TITLE" => "Новости",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => "",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "Y",
+        "AJAX_OPTION_ADDITIONAL" => ""
+        ),
+        false
+    );?>*/-->
+	<p align="justify" class="last"><img src="/upload/medialibrary/614/map_final_2012_shtrih_02_80.jpg" title="map_final_2012_shtrih_02_80.jpg" border="0" alt="map_final_2012_shtrih_02_80.jpg" width="640" height="480"  /></p>
+	<p align="justify" class="last">
+		Учебный центр компании «ШТРИХ-М».
+	</p>
+	<p align="justify" class="last">
+		Адрес: 115280, г. Москва, ул. Ленинская Слобода, д.19,стр.4. «Учебный Центр» компании «ШТРИХ-М» ( 1этаж). Вход с ул. 3-й Автозаводский проезд. (м. Автозаводская, последний вагон из центра, при выходе из стеклянных дверей повернуть налево, выход к "Обувному центру" ( пройти здание насквозь или через проход отеля "Панорама"), далее повернуть налево.
+	</p>
+	<p align="justify" class="last">
+		Руководитель Учебного центра ГК "ШТРИХ-М": Пряхина Ирина<br>
+		Телефон: +7(495)787-60-90 (доб.281)<br>
+		Email: <a href="mailto:ipryakhina@shtrih-m.ru">ipryakhina@shtrih-m.ru</a>, <a href="mailto:tc@shtrih-m.ru">tc@shtrih-m.ru</a>
+	</p>
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
